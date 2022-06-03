22b07367a352b2ad421f554e1ebf2ad7375830bd
sravan <sravan.k.vodnala@gmail.com>
2022-06-03 22:27:44 +0530

    Added:part time employee wage

---
+++ src/UC3PartTime.java
@@ -0,0 +1,10 @@
+public class UC3PartTime {
+    public static void main(String[] args){
+        int Wage_per_hour=20;
+        int Half_day_hour=4;
+        int daily_wage;
+        System.out.println("Employee is present part time ");
+        daily_wage=(Wage_per_hour*Half_day_hour);
+        System.out.println("Employee daily wage is = " +daily_wage);
+    }
+}
