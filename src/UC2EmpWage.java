c4fbae7fd8f426ecfcf3e450c3f38df63474256a
sravan <sravan.k.vodnala@gmail.com>
2022-06-03 22:18:02 +0530

    Added:Calculated daily employee wage

---
+++ src/UC2EmpWage.java
@@ -0,0 +1,12 @@
+public class UC2EmpWage {
+    public static void main(String[] args){
+        int Wage_per_hour=20;
+        int Full_day_hour=8;
+        int daily_Wage;
+            System.out.println("Employee is present in Full day");
+            daily_Wage=(Wage_per_hour*Full_day_hour);
+            System.out.println("Employee daily wage is = "+daily_Wage);
+        
+    }
+}
+
