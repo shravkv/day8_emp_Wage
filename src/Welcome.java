03b754bde4b6c5c68244707c45937a2f258943d9
sravan <sravan.k.vodnala@gmail.com>
2022-06-03 21:52:15 +0530

    Added:welcome to Employee Wage

---
+++ src/Welcome.java
@@ -0,0 +1,8 @@
+public class Welcome {
+    public static void main(String[] args){
+
+        String display="Welcome to Employee wage comutation program on branch master";
+        System.out.println(display);
+    }
+}
+
