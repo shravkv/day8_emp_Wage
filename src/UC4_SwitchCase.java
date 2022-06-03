2c0cbae0badae9eec93c554ef27d1395b25087ca
sravan <sravan.k.vodnala@gmail.com>
2022-06-03 22:48:56 +0530

    Added:Solved using Switch Case

---
+++ src/UC4_SwitchCase.java
@@ -0,0 +1,25 @@
+public class UC4_SwitchCase {
+    public static final int IS_FULL_TIME = 1;
+    public static final int IS_PART_TIME = 2;
+    public static final int EMP_RATE_PER_HOUR = 20;
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation Program");
+        int empHrs = 0;
+        int empWage = 0;
+        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+        switch (empCheck) {
+            case IS_FULL_TIME:
+                empHrs = 8;
+                break;
+            case IS_PART_TIME:
+                empHrs = 4;
+                break;
+            default:
+                empHrs = 0;
+
+        }
+        empWage = empHrs * EMP_RATE_PER_HOUR;
+        System.out.println("Emp Wage: " + empWage);
+    }
+}
+
