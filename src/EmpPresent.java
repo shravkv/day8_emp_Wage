c6ca60d3523e04445e9167347111dc3871d5577a
sravan <sravan.k.vodnala@gmail.com>
2022-06-08 22:57:35 +0530

    Added:calulate emp wage for multiple companies

--- src/EmpPresent.java
+++ src/EmpPresent.java
@@ -1,8 +1,7 @@
 public class EmpPresent {
     public static void main(String[] args) {
-        // Constants
+
         int IS_FULL_TIME = 1;
-        // Computation
         double empCheck = Math.floor(Math.random() *10) % 2;
         if (empCheck == IS_FULL_TIME)
             System.out.println("Employee is Present");
--- src/EmpWageMultiple.java
+++ src/EmpWageMultiple.java
@@ -1,2 +1,36 @@
 public class EmpWageMultiple {
+
+    public static final int IS_PART_TIME = 1;
+    public static final int IS_FULL_TIME = 2;
+
+    public static int computeEmpWage(String company,int empRatePerHour,int numOfworkingDays, int maxHoursPerMonth) {
+
+        int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+
+        while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfworkingDays) {
+            totalWorkingDays++;
+            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+            switch (empCheck) {
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
+            }
+            totalEmpHrs += empHrs;
+            System.out.println("Day#: " + totalWorkingDays + " Emp Hr: " +empHrs);
+        }
+        int totalEmpWage = totalEmpHrs * empRatePerHour;
+        System.out.println("Total Emp wage for company: " +company+" is: "+totalEmpWage);
+        return totalEmpWage;
+    }
+
+    public static void main(String[] args)  {
+        computeEmpWage("Icici", 20, 2, 10);
+        computeEmpWage("Wipro" ,10, 4, 20);
+
+    }
 }
