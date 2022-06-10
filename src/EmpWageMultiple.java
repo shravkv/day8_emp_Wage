8b7fa1d38358c0ec38e23b1224c9e11748ba9e68
sravan <sravan.k.vodnala@gmail.com>
2022-06-10 23:07:00 +0530

    Added:emp wage formultiple companies

--- src/EmpWageMultiple.java
+++ src/EmpWageMultiple.java
@@ -3,6 +3,7 @@ public class EmpWageMultiple {
     public static final int IS_PART_TIME = 1;
     public static final int IS_FULL_TIME = 2;
 
+
     public static int computeEmpWage(String company,int empRatePerHour,int numOfworkingDays, int maxHoursPerMonth) {
 
         int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
